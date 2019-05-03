575536f0d3e9cdddfed5ca8d02f8d14469a7e39c
Noah Huck <nhuck@students.kennesaw.edu>
2019-05-03 02:52:17 -0400

    General Updates

--- src/pkg/Interpreter.java
+++ src/pkg/Interpreter.java
@@ -12,27 +12,49 @@ public class Interpreter {
 
     public static void main(String[] args) {
         //file name is passed in as argument
-         
-        Parser p = new Parser(args[0]);
-        
-        System.out.printf("\nNow parsing from file: %s \n\n", args[0]);
-        
-        while (!p.endOfFileReached()){      //executes until end of file is reached
-            p.parseProgram();
-            // parseProgram parses for 1 complete function at a time
-            // loop will execute until all functions have been parsed
+                 
+        if (args.length > 1){
+            switch (args[0]){
+                case "-p":
+                    System.out.printf("\nNow parsing file %s \n\n", args[1]);
+                    Parser p = new Parser(args[1]);
+                    while (!p.endOfFileReached()){      //executes until end of file is reached
+                        p.parseProgram();
+                        // parseProgram parses for 1 complete function at a time
+                        // loop will execute until all functions have been parsed
+                    }
+                    break;
+                case "-s":
+                    System.out.printf("\nNow Scanning file %s \n\n", args[1]);
+                    Scanner s = new Scanner(new java.io.File(args[1]));
+                    while (!s.endOfFileReached()){      //executes until end of file is reached
+
+                        //next token is converted to a string by convertType function
+                        System.out.printf("%s, row %d, col %d, symbol: %s\n",
+                                s.convertType(s.nextToken()), s.getRow(), s.getCol(), s.currentLexeme);
+                    }
+                    break;
+                case "-e":
+                    System.out.printf("\nNow Executing file %s \n\n", args[1]);
+                    Parser p2 = new Parser(args[1]);
+                    p2.running = true;
+                    while (!p2.endOfFileReached()){      //executes until end of file is reached
+                        p2.parseProgram();
+                        // parser both parses line by line and then executes corresponding function
+                        // loop will execute until all functions have been parsed
+                    }
+            }
+        } else {
+        System.out.printf("\nNow Executing file: %s \n\n", args[0]);
+            Parser p2 = new Parser(args[0]);
+            p2.running = true;
+            while (!p2.endOfFileReached()){      //executes until end of file is reached
+                p2.parseProgram();
+                // parser both parses line by line and then executes corresponding function
+                // loop will execute until all functions have been parsed
+            }
         }
-        
-        System.out.println("\n\n\n\n");
-        
-        Parser p2 = new Parser(args[0]);
-        p2.running = true;
-        while (!p2.endOfFileReached()){      //executes until end of file is reached
-            p2.parseProgram();
-            // parseProgram parses for 1 complete function at a time
-            // loop will execute until all functions have been parsed
-        }
-        System.out.println("\nEnd of file reached.");
+//        System.out.println("\nEnd of file reached.");
 
      //   for testing purposes:
 //        System.out.println("\nIdentifier Table: \n\n");
